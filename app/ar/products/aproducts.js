3cd745e1ac6247811bcf144d854cc76e818d5bac
Asad Al Muharrami <akmuharrami@gmail.com>
2023-10-28 09:03:23 +0400

    ll

--- app/ar/products/aproducts.js
+++ app/ar/products/aproducts.js
@@ -1,6 +1,7 @@
 "use client"
 export default function Aproductss() {
+    return(
     <div style={{background: '#0B0E13', width: "100%", paddingBottom:'2.5%', zIndex:0}}>
         
-    </div>
+    </div>)
 }
--- app/ar/products/page.js
+++ app/ar/products/page.js
@@ -1,4 +1,4 @@
-// import Aproductss from "./apacks";
+import Aproductss from "./aproducts";
 export const metadata = {
   title: "نوفر باقات متعددة لتطوير المواقع",
   description: "نوفر 3 باقات مع توفير خدمة عناية شهرية بأسعار معقولة بالريال العماني",
