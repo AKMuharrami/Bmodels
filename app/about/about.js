8a3bee79f1b025ca636f181e1439ce306a6872d6
Asad Al Muharrami <akmuharrami@gmail.com>
2024-01-29 09:19:30 +0400

    ffdd

--- app/about/about.js
+++ app/about/about.js
@@ -19,42 +19,62 @@ export default function Teamm() {
           <link rel="canonical" href="/about"/>
           </Helmet> */}
         <div style={{background: '#0B0E13', width: "100vw", paddingBottom:'50px', zIndex:-1, height:'8vh'}}></div>
-            {isDesktopOrLaptop &&<div style={{display:'flex', justifyContent:'space-between', background:'#323136', height:'35vh', direction:'ltr'}}>
-            {/* <img src="https://canvas-generations-v1.s3.us-west-2.amazonaws.com/2423baeb-ceab-4d81-8830-d4a4cb3c8bf4.png" alt=""></img>
+            {isDesktopOrLaptop &&<div style={{display:'flex', justifyContent:'center', background:'#323136', height:'45vh', direction:'rtl', textAlign:'center'}}>
+            {/* <img src="https://canvas-generations-v1.s3.us-west-2.amazonaws.com/2423baeb-ceab-4d81-8830-d4a4cb3c8bf4.png" alt=""></img> */}
             <motion.div
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: -20, opacity: 1 }}
             exit={{ y: -10, opacity: 0 }}
             transition={{ duration: 0.3 }}
             style={{marginTop:'1.5vh', width:'70vw', right:0}}>
-                <h2 style={{color:'#F2EEE9', opacity:'1'}}>My name is Asad Al Muharrami <br></br> I live in Muscat - Oman <br></br> I'm self learnt and I beleive that certificates are of no worth<br></br> if they have no work to back them up, as that is all that matters.<br></br><br></br> I'm capable of designing websites in the following languages:</h2>
-                <div style={{marginTop:'-2vh'}}> 
+                <h2 style={{color:'#F2EEE9', opacity:'1'}}>"هل ترغب في الحصول على نموذج ثلاثي الأبعاد جاهز للاستخدام في مادة تقنية المعلومات؟ 
+<br></br><br></br>
+🔴 نحن نقدم لك نموذجًا ثلاثي الأبعاد مصممًا خصيصًا لطلاب الصف العاشر، يمكن استخدامه في الفصول الدراسية كمشروع للحصول على اعلى الدرجات أو كأداة تعليمية مفيدة. 
+
+🔴 يتضمن النموذج تصاميم واقعية للسمكة الموجوده في المنهج الدراسي ولأجهزة الكمبيوتر والشبكات والبرمجيات والمزيد، مما يمكّن الطلاب من الحصول على الدرجة كامله وتطبيق المفاهيم التعليمية بطريقة عملية ومشوقة. 
+
+
+🔴 يمكنكم الآن الحصول على نموذج ثلاثي الأبعاد جاهز للتعديل والاستخدام في فصلكم الدراسي بأسعار رمزية. 
+<br></br><br></br>
+⚫️تواصل معانا الآن 📱لمعرفة المزيد عن خدماتنا وكيف يمكننا مساعدتك ولا تفوّتوا هذه الفرصة
+</h2><h2 style={{color:'#F2EEE9', opacity:'1'}}><a style={{color:"darkcyan"}} href="https://wa.me/96895455338">https://wa.me/96895455338</a></h2>
+                {/* <div style={{marginTop:'-2vh'}}> 
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"18px", height:'7vh',width:'10vw',textAlign:'center',lineHeight:3}}>HTML</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"18px", height:'7vh',width:'10vw',textAlign:'center',lineHeight:3}}>CSS</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"18px", height:'7vh',width:'10vw',textAlign:'center',lineHeight:3}}>Javascript</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"18px", height:'7vh',width:'10vw',textAlign:'center',lineHeight:3}}>React</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"18px", height:'7vh',width:'15vw',textAlign:'center',lineHeight:3}}>Various related libraries</h3></button>
-                     </div>
+                     </div> */}
             </motion.div>
-                    <br></br> */}
+                    <br></br>
             </div>}
-            {isTabletOrMobile &&<div style={{display:'flex', justifyContent:'space-between', background:'#323136', height:'20vh'}}>
-            {/* <img src="https://canvas-generations-v1.s3.us-west-2.amazonaws.com/2423baeb-ceab-4d81-8830-d4a4cb3c8bf4.png"  alt="" style={{width:'28vw', objectFit:'cover'}}></img>
+            {isTabletOrMobile &&<div style={{display:'flex', justifyContent:'center', background:'#323136', height:'43vh', textAlign:'center'}}>
+            {/* <img src="https://canvas-generations-v1.s3.us-west-2.amazonaws.com/2423baeb-ceab-4d81-8830-d4a4cb3c8bf4.png"  alt="" style={{width:'28vw', objectFit:'cover'}}></img> */}
             <motion.div
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: -20, opacity: 1 }}
             exit={{ y: -10, opacity: 0 }}
             transition={{ duration: 0.3 }}
             style={{marginTop:'1.5vh'}}>
-                <h4 style={{color:'#F2EEE9', opacity:'1', fontSize:'1.4vh', marginRight:'1vw', marginLeft:'1vw'}}>My name is Asad Al Muharrami <br></br> I live in Muscat - Oman <br></br> I'm self learnt by the blessing of Allah and I beleive that certificates are of no worth if they have no work to back them up, as that is all that matters.<br></br><br></br> I'm capable of designing websites in the following languages:</h4>
-                <div style={{marginTop:'-1.3vh'}}> 
+                <h4 className="cairo" style={{color:'#F2EEE9', opacity:'1', fontSize:'1.4vh', marginRight:'1vw', marginLeft:'1vw'}}>"هل ترغب في الحصول على نموذج ثلاثي الأبعاد جاهز للاستخدام في مادة تقنية المعلومات؟ 
+<br></br><br></br>
+🔴 نحن نقدم لك نموذجًا ثلاثي الأبعاد مصممًا خصيصًا لطلاب الصف العاشر، يمكن استخدامه في الفصول الدراسية كمشروع للحصول على اعلى الدرجات أو كأداة تعليمية مفيدة. 
+
+🔴 يتضمن النموذج تصاميم واقعية للسمكة الموجوده في المنهج الدراسي ولأجهزة الكمبيوتر والشبكات والبرمجيات والمزيد، مما يمكّن الطلاب من الحصول على الدرجة كامله وتطبيق المفاهيم التعليمية بطريقة عملية ومشوقة. 
+
+
+🔴 يمكنكم الآن الحصول على نموذج ثلاثي الأبعاد جاهز للتعديل والاستخدام في فصلكم الدراسي بأسعار رمزية. 
+<br></br><br></br>
+⚫️تواصل معانا الآن 📱لمعرفة المزيد عن خدماتنا وكيف يمكننا مساعدتك ولا تفوّتوا هذه الفرصة
+</h4><h4 className="cairo" style={{color:'#F2EEE9', opacity:'1'}}><a style={{color:"darkcyan"}} href="https://wa.me/96895455338">https://wa.me/96895455338</a></h4>
+                {/* <div style={{marginTop:'-1.3vh'}}> 
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"7px", height:'2vh',width:'7vw',textAlign:'center',lineHeight:2.2}}>HTML</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"7px", height:'2vh',width:'7vw',textAlign:'center',lineHeight:2.2}}>CSS</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"7px", height:'2vh',width:'8vw',textAlign:'center',lineHeight:2.2}}>Javascript</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"7px", height:'2vh',width:'7vw',textAlign:'center',lineHeight:2.2}}>React</h3></button>
                     <button style={{background:'rgb(255, 255, 255, 0)', border:'0', }}><h3 style={{background:'#F4E6C3',borderRadius:'50px', fontSize:"7px", height:'2vh',width:'13vw',textAlign:'center',lineHeight:2.2}}>Related libraries</h3></button>
-                     </div>
+                     </div> */}
             </motion.div>
-                    <br></br> */}
+                    <br></br>
             </div>}
     </div>)}
